1598c919e02cfe1f92675546ae06d39196e3ea6e
Michael Foord <mfoord@python.org>
2010-07-20 14:20:26 +0100

    Fix events test

--- unittest2/test/test_events.py
+++ unittest2/test/test_events.py
@@ -1,11 +1,12 @@
 import unittest2
 
+from collections import deque
 
 class TestEvents(unittest2.TestCase):
     
     def test_eventhook(self):
         hook = unittest2.events._EventHook()
-        self.assertEqual(hook._handlers, [])
+        self.assertEqual(hook._handlers, deque())
         
         self.called = False
         event = object()
@@ -16,25 +17,30 @@ class TestEvents(unittest2.TestCase):
             return return_val
         
         hook += handler1
-        hook(event)
+        result = hook(event)
+        self.assertFalse(result)
         self.assertTrue(self.called)
         
         self.called = False
         def handler2(arg):
             self.assertIs(arg, event)
             return return_val
-        hook(event)
+        hook += handler2
+        
+        result = hook(event)
+        self.assertFalse(result)
         self.assertTrue(self.called)
         
         self.called = False
-        return_val = True
-        hook(event)
+        return_val = object()
+        result = hook(event)
+        self.assertIs(result, return_val)
         self.assertFalse(self.called)
         
         hook -= handler1
         hook -= handler2
         hook(object())
-        self.assertEqual(hook._handlers, [])
+        self.assertEqual(hook._handlers, deque())
 
 
 if __name__ == '__main__':
