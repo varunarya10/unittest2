bec886ff37ca493a7232dba614f964a118bb7ed7
Michael Foord <mfoord@python.org>
2010-07-22 23:35:04 +0100

    Addition of setUp and tearDown decorators to moduleloading extension.

--- unittest2/plugins/moduleloading.py
+++ unittest2/plugins/moduleloading.py
@@ -18,8 +18,27 @@ def loadModules(event):
         if isinstance(obj, type) and issubclass(obj, unittest2.TestCase):
             tests.append(loader.loadTestsFromTestCase(obj))
         elif isinstance(obj, types.FunctionType) and is_test(obj):
-            case = unittest2.FunctionTestCase(obj)
+            args = {}
+            setUp = getattr(obj, 'setUp', None)
+            tearDown = getattr(obj, 'tearDown', None)
+            if setUp is not None:
+                args['setUp'] = setUp
+            if tearDown is not None:
+                args['tearDown'] = tearDown
+            case = unittest2.FunctionTestCase(obj, **args)
             tests.append(case)
     return loader.suiteClass(tests)
 
 hooks.loadTestsFromModule += loadModules
+
+def setUp(setupFunction):
+    def decorator(func):
+        func.setUp = setupFunction
+        return func
+    return decorator
+
+def tearDown(tearDownFunction):
+    def decorator(func):
+        func.tearDown = tearDownFunction
+        return func
+    return decorator
