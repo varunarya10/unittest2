7084664957efcdb0b95d68990b9d2c1282fd34ee
Michael Foord <michael@voidspace.org.uk>
2010-07-25 00:29:49 +0100

    Progress in junit xml generation

--- unittest2/plugins/junitxml.py
+++ unittest2/plugins/junitxml.py
@@ -1,5 +1,7 @@
 from unittest2.events import Plugin, addOption
 
+def xmlEscape(value):
+    return value
 
 class JunitXml(Plugin):
     
@@ -8,13 +10,73 @@ class JunitXml(Plugin):
     
     def __init__(self):
         self.path = self.config.as_bool('path', default='junit.xml')
+        self.errors = 0
+        self.failed = 0
+        self.skipped = 0
+        self.numtests = 0
+        self.test_logs = []
 
     def startTest(self, event):
-        pass
+        self.numtests += 1
 
     def stopTest(self, event):
-        pass
+        d = {'time': "%.6f" % event.timeTaken}
+        test = event.test
+        classnames = ('%s.%s' % (test.__module__, 
+                                 test.__class__.__name__)).split('.')
+        d['classname'] = ".".join(classnames)
+        d['name'] = xmlEscape(test._testMethodName)
+        attrs = ['%s="%s"' % item for item in sorted(d.items())]
+        
+        self.test_logs.append('<testcase %s>' % ' '.join(attrs))
+        
+        msg = ''
+        if event.exc_info:
+            msg = xmlEscape(event.exc_info[1])
+        if event.error:
+            self.errors += 1
+            self.test_logs.append(
+                '<error message="test failure">%s</error>' % msg
+            )
+        elif event.failed:
+            self.failed += 1
+            self.test_logs.append(
+                '<failure message="test failure">%s</failure>' % msg
+            )
+        elif event.unexpectedSuccess:
+            self.skipped += 1
+            self.test_logs.append(
+                '<skipped message="test passes unexpectedly"/>'
+            )
+        elif event.skipped:
+            self.skipped += 1
+            self.test_logs.append("<skipped/>")
+        elif event.expectedFailure:
+            self.skipped += 1
+            self.test_logs.append(
+                '<skipped message="expected test failure">%s</skipped>' % msg
+            )
+        
+        self.test_logs.append("</testcase>")
+        
 
     def stopTestRun(self, event):
-        # probably not valid xml
-        open(self.path, 'w').write('foo')
+        data = []
+        data.append('<?xml version="1.0" encoding="utf-8"?>')
+        data.append('<testsuite ')
+        data.append('name="" ')
+        data.append('errors="%i" ' % self.errors)
+        data.append('failures="%i" ' % self.failed)
+        data.append('skips="%i" ' % self.skipped)
+        data.append('tests="%i" ' % self.numtests)
+        data.append('time="%.3f"' % event.timeTaken)
+        data.append(' >')
+        data.extend(self.test_logs)
+        data.append('</testsuite>')
+        
+        handle = open(self.path, 'w')
+        try:
+            handle.write('\n'.join(data))
+            handle.write('\n')
+        finally:
+            handle.close()
