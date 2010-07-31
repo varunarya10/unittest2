66c5a083ac4e9eb7b82ed9f1f5ffd2876dd26115
Michael Foord <michael@voidspace.org.uk>
2010-07-31 20:21:49 +0100

    Additions to config file

--- unittest2/plugins/timed.py
+++ unittest2/plugins/timed.py
@@ -2,10 +2,12 @@ from unittest2.events import Plugin
 
 import time
 
+help_text = 'Output time taken for each test (verbose only)'
+
 class TimedTests(Plugin):
     
     configSection = 'timed'
-    commandLineSwitch = ('T', 'timed', 'Output time taken for each test')
+    commandLineSwitch = ('T', 'timed', help_text)
 
     def __init__(self):
         self.threshold = self.config.as_float('threshold', 0)
@@ -13,4 +15,5 @@ class TimedTests(Plugin):
     def stopTest(self, event):
         if event.timeTaken >= self.threshold:
             msg = '  %.2f seconds  ' % event.timeTaken
+            # only output in verbose (verbosity = 2)
             event.message(msg, 2)
