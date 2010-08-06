877a2cc7bb7b73b1ad84e19c9a712fdedce2d2c9
Michael Foord <michael@voidspace.org.uk>
2010-08-06 16:14:59 +0100

    Addition of parameterised tests to moduleloading plugin

--- unittest2/plugins/moduleloading.py
+++ unittest2/plugins/moduleloading.py
@@ -19,9 +19,17 @@ def testGenerator(func):
     func.testGenerator = True
     return func
 
+def params(*paramList):
+    def decorator(func):
+        func.paramList = paramList
+        return func
+    return decorator
+
+
 class Functions(Plugin):
     
     generatorsEnabled = False
+    parametersEnabled = False
     configSection = 'functions'
     commandLineSwitch = (None, 'functions', 'Load tests from functions')
 
@@ -46,22 +54,30 @@ class Functions(Plugin):
                 if tearDown is not None:
                     args['tearDown'] = tearDown
                 
-                if (not self.generatorsEnabled or 
-                    getattr(obj, 'testGenerator', None) is None):
-                    case = FunctionTestCase(obj, **args)
-                    tests.append(case)
-                else:
+                paramList = getattr(obj, 'paramList', None)
+                isGenerator = getattr(obj, 'testGenerator', False)
+                if self.parametersEnabled and paramList is not None:
+                    for index, argSet in enumerate(paramList):
+                        def func(argSet=argSet, obj=obj):
+                            return obj(*argSet)
+                        name = '%s.%s' % (obj.__module__, obj.__name__)
+                        func_name = name_from_args(name, index, argSet)
+                        case = ParamsFunctionCase(func_name, func, **args)
+                        tests.append(case)
+                elif self.generatorsEnabled and isGenerator:
                     extras = list(obj())
                     name = '%s.%s' % (obj.__module__, obj.__name__)
                     def createTest(name):
                         return GeneratorFunctionCase(name, **args)
                     tests.extend(testsFromGenerator(name, extras, createTest))
+                else:
+                    case = FunctionTestCase(obj, **args)
+                    tests.append(case)
                 
         event.extraTests.extend(tests)
 
 
 class GeneratorFunctionCase(FunctionTestCase):
-
     def __init__(self, name, **args):
         self._name = name
         FunctionTestCase.__init__(self, None, **args)
@@ -74,6 +90,15 @@ class GeneratorFunctionCase(FunctionTestCase):
 
     id = __str__ = __repr__
 
+class ParamsFunctionCase(FunctionTestCase):
+    def __init__(self, name, func, **args):
+        self._name = name
+        FunctionTestCase.__init__(self, func, **args)
+        
+    def __repr__(self):
+        return self._name
+
+    id = __str__ = __repr__
 
 class Generators(Plugin):
 
@@ -104,9 +129,7 @@ class Generators(Plugin):
 
 def testsFromGenerator(name, tests, testCaseClass):
     for index, (func, args) in enumerate(tests):
-        summary = ', '.join(repr(arg) for arg in args)
-
-        method_name = '%s_%s\n%s' % (name, index + 1, summary[:79])
+        method_name = name_from_args(name, index, args)
         setattr(testCaseClass, method_name, None)
         instance = testCaseClass(method_name)
         delattr(testCaseClass, method_name)
@@ -114,3 +137,31 @@ def testsFromGenerator(name, tests, testCaseClass):
             return func(*args)
         setattr(instance, method_name, method)
         yield instance
+
+def name_from_args(name, index, args):
+    summary = ', '.join(repr(arg) for arg in args)
+    return '%s_%s\n%s' % (name, index + 1, summary[:79])
+
+
+class Parameters(Plugin):
+    configSection = 'parameters'
+    commandLineSwitch = (None, 'params', 'Enable parameterised tests')
+
+    def pluginsLoaded(self, event):
+        Functions.parametersEnabled = True
+
+    def getTestCaseNames(self, event):
+        names = filter(event.isTestMethod, dir(event.testCase))
+        klass = event.testCase
+        for name in names:
+            method = getattr(klass, name)
+            paramList = getattr(method, 'paramList', None)
+            if paramList is None:
+                continue
+
+            event.excludedNames.append(name)
+            for index, args in enumerate(method.paramList):
+                def _method(self, method=method, args=args):
+                    return method(self, *args)
+                method_name = name_from_args(name, index, args)
+                setattr(klass, method_name, _method)
