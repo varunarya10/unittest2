410792de949cbe11fe7382c561566cf937ed06d2
Ezio Melotti <ezio.melotti@gmail.com>
2012-11-08 11:08:39 +0200

    #16433: fix docstring of assertNotEqual.

--- unittest2/case.py
+++ unittest2/case.py
@@ -651,7 +651,7 @@ class TestCase(unittest.TestCase):
         assertion_func(first, second, msg=msg)
 
     def assertNotEqual(self, first, second, msg=None):
-        """Fail if the two objects are equal as determined by the '=='
+        """Fail if the two objects are equal as determined by the '!='
            operator.
         """
         if not first != second:
