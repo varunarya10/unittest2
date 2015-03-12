f4fa148ea175870fc4ab9aac3ffd1698e12bd4aa
Robert Collins <rbtcollins@hp.com>
2015-03-12 15:42:28 +1300

    unittest2 issue #94: version the dep on six.
    We use six.range, added in 1.4.

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ KEYWORDS = "unittest testing tests".split(' ')
 # Both install and setup requires - because we read VERSION from within the
 # package, and the package also exports all the APIs.
 # six for compat helpers
-REQUIRES = ['argparse', 'six', 'traceback2'],
+REQUIRES = ['argparse', 'six>=1.4', 'traceback2'],
 
 params = dict(
     name=NAME,
